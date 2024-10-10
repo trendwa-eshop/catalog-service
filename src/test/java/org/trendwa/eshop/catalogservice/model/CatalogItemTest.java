256db011ffe525529d52796c66fe93cfb1254208
Musa BEYTEKIN <musa.beytekin1@gmail.com>
2024-10-10 23:38:05 +0300

    fix: #44

--- src/test/java/org/trendwa/eshop/catalogservice/model/CatalogItemTest.java
+++ src/test/java/org/trendwa/eshop/catalogservice/model/CatalogItemTest.java
@@ -80,7 +80,7 @@ class CatalogItemTest {
                       "onReorder": false
                     }
                     """;
-            assertThat(json.parseObject(expected))
+            assertThat(json.parseObject(expected)).usingRecursiveComparison()
                     .isEqualTo(catalogItems[0]);
         }
 
